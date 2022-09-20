f59575398ff6f69039ae8e3b1621d8b4545b3196
Kyle Dixon <KyleDixon@hotmail.ca>
2022-09-20 11:38:19 -0300

    Created folders for future organization

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float maxSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // accelerate until player hits max speed -- slow on collision with obstacle
+    }
+}
